06475bf8df2ab9c14080587e5e98d2e63f088313
Kamil Gajewski <kwa89@interia.pl>
2020-03-05 00:36:20 +0100

    Updates

---
+++ precache-manifest.c85084c48988645986c0fcc658ef5b4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d16444fc5a175826b6a9",
+    "url": "/app-react-router/static/css/main.37f6d986.chunk.css"
+  },
+  {
+    "revision": "d16444fc5a175826b6a9",
+    "url": "/app-react-router/static/js/main.1e68182c.chunk.js"
+  },
+  {
+    "revision": "d466ce07c2972432d57f",
+    "url": "/app-react-router/static/js/runtime~main.6a4469d8.js"
+  },
+  {
+    "revision": "b387d51cae3efefea11a",
+    "url": "/app-react-router/static/js/2.09b6eb77.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/app-react-router/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/app-react-router/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/app-react-router/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "97719cee2be16b2f392e36d2a8cbeef9",
+    "url": "/app-react-router/index.html"
+  }
+];
